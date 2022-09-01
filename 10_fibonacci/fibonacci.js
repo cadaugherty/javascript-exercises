63567fd4668649f71481ca1bfbd29530b0e7681a
Chris <chris.a.daugherty@gmail.com>
2022-09-01 19:24:18 -0400

    easy

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num1) {
+    num1 = parseInt(num1);
+    fib = [1,1];
+    if(num1 >0){
+    for(i=1; i<num1; i++){
+        fib.push(fib[i] + fib[i-1]);
+     }
+        return fib[i-1];
+    } else {
+        return "OOPS";
+    }
 
 };
 
