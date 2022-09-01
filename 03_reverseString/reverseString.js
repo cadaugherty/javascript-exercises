adb5581c7d272578cf870fdd95711bda576df1cf
Chris <chris.a.daugherty@gmail.com>
2022-09-01 18:02:23 -0400

    Done with this section

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    const stringArray = stringToReverse.split("");
+    const targetArray = [];
+    while (stringArray.length > 0){
+        targetArray.push(stringArray.pop());
+    };
+    let answerString = targetArray.join("");
+    return answerString;
 };
 
 // Do not edit below this line
