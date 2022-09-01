cfbe6fb29b70c2972b1c66fe83cf9267f15bd893
Chris <chris.a.daugherty@gmail.com>
2022-09-01 19:18:06 -0400

    solved, finally

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+const palindromes = function (stringTest) {
+    stringTest = stringTest.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let stringArray = Array.from(stringTest);
 
+    let backwardsString = [];
+    for(i = 0; i < stringArray.length; i++){
+        if(stringArray[i] == " "){
+            stringArray.splice(i, 1);
+        }
+    };
+
+    for(i=stringArray.length-1; i>=0; i--){
+        backwardsString.push(stringArray[i]);
+    };
+    
+    string1 = stringArray.join("");
+    string2 = backwardsString.join("");
+    string1 = string1.toLowerCase();
+    string2 = string2.toLowerCase();
+   
+    if(string1 === string2){
+        return true;
+    } else{
+        return false;
+    };
+   
 };
 
 // Do not edit below this line
