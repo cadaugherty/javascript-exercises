305bd91ffcd8e437159ea22027a1f55adbdbdeae
Chris <chris.a.daugherty@gmail.com>
2022-09-01 18:31:18 -0400

    solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(yearToCheck) {
+    if(yearToCheck % 100 == 0){
+        if(yearToCheck % 400 == 0){
+            return true;
+        } else{
+            return false;
+        }
+    } else if(yearToCheck % 4 == 0){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
