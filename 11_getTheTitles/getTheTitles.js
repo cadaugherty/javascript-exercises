83103361cfbe36cf0a7bf60ec4db721b771cc8a7
Chris <chris.a.daugherty@gmail.com>
2022-09-01 19:29:56 -0400

    solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    const answer = [];
+    let intermediate = [];
+    for (i=0; i<bookArray.length; i++){
+        intermediate = bookArray[i];
+        answer.push(intermediate.title);
+    }
+    return answer;
 
 };
 
