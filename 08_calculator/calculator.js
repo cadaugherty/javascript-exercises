50f096f11278cd492642a77d9385f71851819e9e
Chris <chris.a.daugherty@gmail.com>
2022-09-01 18:47:23 -0400

    solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,44 @@
-const add = function() {
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+	
+  return num1 - num2;
+};
+
+const sum = function(sumArray) {
+  if(sumArray.length == 0){
+    return 0;
+  }
+  else{
+    let answer = 0;
+    for(i = 0; i < sumArray.length; i++){
+      answer += sumArray[i];
+    };
+    return answer;
+  }
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(mulArray) {
+  let answer = 1;
+  for(i=0; i< mulArray.length; i++) {
+    answer *= mulArray[i];
+  }
+  return answer;
 };
 
-const sum = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1,num2);
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num1) {
+  let answer = 1;
+  for(i=num1; i>0; i--){
+    answer *= i;
+  }
+  return answer;
 	
 };
 
