956fe704854fac2199d1fe3fc7207ebc3f2f1a4f
Chris <chris.a.daugherty@gmail.com>
2022-09-01 17:46:36 -0400

    exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numTimes) {
+    let intermediateString = ""
+    if(numTimes < 0) {
+        return "ERROR";
+    }
+        else{ for(i = 0; i < numTimes; i++){
+            intermediateString += stringToRepeat;
+        };
+        return intermediateString
+    }
 };
 
 // Do not edit below this line
