a0b3026c2c2a6348fc9f960f244f3784e8ee0520
Chris <chris.a.daugherty@gmail.com>
2022-09-01 18:26:08 -0400

    solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let answer = 0;
+    if(Number.isInteger(num1) == false || Number.isInteger(num2) == false){
+        return "ERROR";
+    }
+    else if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    else {
+        for(i = Math.min(num1, num2); i <= Math.max(num1,num2); i++){
+            answer += i;
+        }
+        return answer;
+    };
 
 };
 
